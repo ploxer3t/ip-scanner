c1546bd3335754079be7617e5f9a50feda715118
Ploxeret <140412334+ploxer3t@users.noreply.github.com>
2023-10-16 20:25:37 +0300

    Update ip-info.py

--- ip-info.py
+++ ip-info.py
@@ -4,25 +4,19 @@ from termcolor import colored
 def get_ip_info(ip_address):
     """Gets the IP address information."""
 
-    # Make a request to the IP address info API.
     response = requests.get("https://ipinfo.io/{}/json".format(ip_address))
 
-    # Parse the JSON response.
     data = response.json()
 
-    # Return the IP address information.
     return data
 
 def main():
     """Gets the IP address information and prints it to the console."""
-
-    # Get the IP address from the user.
+    
     ip_address = input("Enter an IP address: ")
 
-    # Get the IP address information.
     ip_info = get_ip_info(ip_address)
 
-    # Print the IP address information to the console.
     print(colored("IP", "red"), "->", colored(ip_info["ip"], "yellow"))
     print(colored("City", "red"), "->", colored(ip_info["city"], "yellow"))
     print(colored("Region", "red"), "->", colored(ip_info["region"], "yellow"))
